acb46c267297382fe617a99dc3b064f273fd4a62
Rahul <rahulkothuri67@gmail.com>
2025-01-31 10:27:15 +0530

    comments

--- model.py
+++ model.py
@@ -4,24 +4,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 
-# Load the dataset
+
 data = pd.read_csv("data\Crop_recommendation.csv")
 
-# Splitting features and target
-X = data.drop(columns=["label"])  # Features
-y = data["label"]  # Target
 
-# Train-test split
+X = data.drop(columns=["label"]) 
+y = data["label"]
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Model training
+
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Test model
+
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
-# Save the trained model
 with open("crop_model.pkl", "wb") as f:
     pickle.dump(model, f)
